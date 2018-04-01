b751d5e7e0cba0d7ec2f6d02775c2760c9358a70
Simeon <adegbolasimeon@gmail.com>
2018-04-01 12:32:54 +0100

    refactored types to different file

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import Types
+  ( Element (..)
+  , Line
+  , Board
+  , Game (..)
+  , Position
+  , Input
+  )
+
 import Data.Bool (bool)
 import Data.List (intersperse, findIndex)
 import Data.List.Split (endBy)
@@ -9,49 +18,6 @@ import Control.Monad (guard)
 main :: IO ()
 main = do
   putStrLn "hello world"
-
-data Element = Wall
-             | Player
-             | PlayerOnGoal
-             | Box
-             | BoxOnGoal
-             | Goal
-             | Floor deriving (Eq)
-
-instance Show Element where
-  show Wall         = "#"
-  show Player       = "@"
-  show PlayerOnGoal = "+"
-  show Box          = "$"
-  show BoxOnGoal    = "*"
-  show Goal         = "."
-  show Floor        = " "
-
-{-
-each line on the board is a list of elements i.e
-#### ^-- line 1
-#@.# ^-- line 2
-#$ # ^-- line 3 etc
-#  #
-# $#
-#. #
-#  #
-####
-
-the board is a list of lines
--}
-type Line = [Element]
-type Board = [Line]
-data Game = Game  { currentBoard :: Board,
-                    playerPosition :: (Int, Int)
-                  }
-instance Show Game where
-  show game = concat boardLines
-    where
-      -- ["##", "##"] -> ["##", "\n", "##"]
-      boardLines = intersperse "\n" board'
-      -- [[Wall, Wall]] -> [["#", "#"]] -> [["##"]]
-      board' = concatMap show <$> currentBoard game
 
 toGameElement :: Char -> Element
 toGameElement '#' = Wall
@@ -85,17 +51,13 @@ readRest left | null left = []
 initLevel :: Board -> Game
 initLevel board = Game {currentBoard = board, playerPosition = go 0 board}
  where
-  -- if the player is on the line indicated by the index, find column
-  -- else check the next line
+    -- if the player is on the line indicated by the index, find column
+    -- else check the next line
   go :: Int -> Board -> Position
   go index (x:xs) = if Player `elem` x || PlayerOnGoal `elem` x
     then (index, findCol x)
     else go (index + 1) xs
   findCol = fromJust . findIndex (\e -> e == Player || e == PlayerOnGoal)
-
-
-type Input = String
-type Position = (Int, Int)
 
 play :: Game -> IO ()
 play game = do
@@ -113,10 +75,8 @@ getPlayerInput = do
 isValidInput :: Input -> Bool
 isValidInput i = i `elem` ["u", "d", "l", "r"]
 
-
 move :: Monad m => Input -> Position -> Board -> m Game
 move = undefined
-
 
 getElement :: Position -> Input -> Board -> Element
 getElement (line, col) "u" board = (board !! (line - 1)) !! col -- ^ previous line, same col
---
+++ src/Types.hs
@@ -0,0 +1,62 @@
+module Types
+  ( Element (..)
+  , Line
+  , Board
+  , Game (..)
+  , Input
+  , Position
+  ) where
+
+import Data.List (intersperse)
+
+data Element = Wall
+             | Player
+             | PlayerOnGoal
+             | Box
+             | BoxOnGoal
+             | Goal
+             | Floor deriving (Eq)
+
+instance Show Element where
+  show Wall         = "#"
+  show Player       = "@"
+  show PlayerOnGoal = "+"
+  show Box          = "$"
+  show BoxOnGoal    = "*"
+  show Goal         = "."
+  show Floor        = " "
+
+type Line = [Element]
+
+type Board = [Line]
+
+
+-- | each line on the board is a list of elements i.e
+{-  
+    #### ^-- line 1
+    #@.# ^-- line 2
+    #$ # ^-- line 3 etc
+    #  #
+    # $#
+    #. #
+    #  #
+    ####
+
+    the board is a list of lines
+-}
+
+data Game = Game  { currentBoard :: Board,
+                    playerPosition :: (Int, Int)
+                  }
+
+instance Show Game where
+  show game = concat boardLines
+    where
+      -- ["##", "##"] -> ["##", "\n", "##"]
+      boardLines = intersperse "\n" board'
+      -- [[Wall, Wall]] -> [["#", "#"]] -> [["##"]]
+      board' = concatMap show <$> currentBoard game
+
+type Input = String
+
+type Position = (Int, Int)
